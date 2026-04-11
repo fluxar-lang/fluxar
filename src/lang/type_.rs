28e0a7aebb41df2637d7eac164f5546fbaf41903
qaltx <67969197+ntqltx@users.noreply.github.com>
2026-04-11 20:13:16 +0300

    chore: cleaned up everything

--- src/lang/type_.rs
+++ src/lang/type_.rs
@@ -1,14 +1,16 @@
 #[derive(Debug, PartialEq, Clone)]
 pub enum Type {
-    Int, String, Class(Box<Type>),
-    Generic(String),
+	Int,
+	String,
+	Class(Box<Type>),
+	Generic(String),
 }
 impl Type {
-    pub fn from_str(type_str: &str) -> Result<Type, String> {
-        match type_str {
-            "int" => Ok(Type::Int),
-            "string" => Ok(Type::String),
-            _ => Ok(Type::Generic(type_str.to_string())),
-        }
-    }
+	pub fn from_str(type_str: &str) -> Result<Type, String> {
+		match type_str {
+			"int" => Ok(Type::Int),
+			"string" => Ok(Type::String),
+			_ => Ok(Type::Generic(type_str.to_string())),
+		}
+	}
 }
